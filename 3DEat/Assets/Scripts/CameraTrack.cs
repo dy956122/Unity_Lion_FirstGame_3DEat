6a1863fab46a3dce85d9a837878c08a87be7c771
dy956122 <dy956122@gmail.com>
2020-05-26 16:20:25 +0800

    玩家、攝影機與GM部分功能
    1. 隨機產生 Prefabs 物件
    2. 讓角色可以 位移、有動畫
    3. 追加攝影機跟蹤
    4. 時間倒數

---
+++ 3DEat/Assets/Scripts/CameraTrack.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+
+    #region 欄位 與屬性
+    /// <summary>
+    /// 玩家變形元件
+    /// </summary>
+    private Transform player;
+
+    [Header("追蹤速度"), Range(0.1f, 50.5f)]
+    public float speed = 1.5f;
+
+
+    #endregion 欄位 與屬性 結束
+
+    #region 方法
+    /// <summary>
+    ///  追蹤玩家
+    /// </summary>
+    private void Track()
+    {
+        // 以下註解，每個人的距離都不一樣
+        // 攝影機 與小明 Y軸 距離  6 - 0.05 = 5.95 
+        // 攝影機 與小明 Z軸 距離  -8 - (-2.8) = -5.2
+        Vector3 posTrack = player.position;
+        posTrack.y += 4.95f;
+        posTrack.z += -4.2f;
+
+        // 攝影機座標 = 變形.座標
+        Vector3 posCam = transform.position;
+        // 攝影機座標 = 三維向量.插值(A點,B點,百分比)
+        posCam = Vector3.Lerp(posCam, posTrack, 0.5f * Time.deltaTime * speed);
+        //  變形.座標 = 攝影機座標
+        transform.position = posCam;
+    }
+    #endregion 方法 結束
+
+
+    #region 事件
+    private void Start()
+    {
+        // 小明物件 = 遊戲物件.尋找("物件名稱").變形
+        player = GameObject.Find("小明").transform;
+    }
+
+
+    // 延遲更新：會在Update 執行後再執行
+    // 建議：需要追蹤座標，要寫在此事件內
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+
+
+
+
+    /* 實驗 Lerp 插值
+    public float A = 0;
+    public float B = 100;
+
+    public Vector2 v2A = Vector2.zero;
+    public Vector2 v2B = Vector2.one * 1000;
+
+
+
+    private void Update()
+    {
+        // Lerp 插值
+        A = Mathf.Lerp(A, B, 0.5f);
+
+        v2A = Vector2.Lerp(v2A, v2B, 0.5f);
+    }*/
+
+    #endregion 事件 結束
+}
---
+++ 3DEat/Assets/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    #region 欄位與屬性
+    [Header("道具")]
+    public GameObject[] props;
+    [Header("文字介面：道具數量")]
+    public Text textcount;
+    [Header("文字介面：倒數時間")]
+    public Text textTime;
+    [Header("文字介面：結束畫面標題")]
+    public Text textTitle;
+    [Header("結束畫面")]
+    public CanvasGroup final;
+    
+
+
+    /// <summary>
+    /// 道具總數
+    /// </summary>
+    private int countTotal;
+
+    /// <summary>
+    /// 取得道具數量
+    /// </summary>
+    private int countProp;
+
+    /// <summary>
+    /// 遊戲時間
+    /// </summary>
+    private float gameTime = 30;
+
+
+
+    #endregion 欄位與屬性 結束
+
+    #region 方法
+
+    /// <summary>
+    /// 生成道具
+    /// </summary>
+    /// <param name="Prop">想要生成的道具數量</param>
+    /// <param name="count">想要生成的道具數量 + 隨機值 + - 5 </param>
+    private int CreateProp(GameObject prop, int count) // 注意：這裡不是 void , 而是 int
+    {
+        // 取得隨機道具數量 = 指定的數量 + - 5
+        int total = count + Random.Range(-5, 5);
+
+        // for 迴圈
+        for (int i = 0; i < total; i++)
+        {
+            // 座標 = (隨機, 1.5 , 隨機)
+            Vector3 pos = new Vector3(Random.Range(-9, 9), 1.5f, Random.Range(-9, 9));
+            // 生成 (物件, 座標, 角度)
+            Instantiate(prop, pos, Quaternion.identity);
+
+        }
+
+        // 傳回 道具數量
+        return total;
+    }
+
+    /// <summary>
+    /// 時間倒數
+    /// </summary>
+    private void CountTime()
+    {
+        // 遊戲時間 遞減 一禎的時間
+        gameTime -= Time.deltaTime;
+
+        // 更新倒數時間介面 ToString ("f小數點位數")
+        textTime.text = "倒數時間：" + gameTime.ToString("f2");
+    }
+
+    #endregion 方法 結束
+
+
+    #region 事件
+    private void Start()
+    {
+        countTotal = CreateProp(props[0], 20); // 道具總數 = 生成道具 (道具一號, 指定數量)
+        textcount.text = "道具數量：0 / " + countTotal;
+
+        CreateProp(props[1], 10); //  生成道具(道具二號, 指定數量)
+
+    }
+
+    private void Update()
+    {
+        CountTime();
+    }
+
+    #endregion 事件 結束
+}
